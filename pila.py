efaac0603dc061afffde0dabfa717de6b2687ac1
walter <belwalterv@gmail.com>
2021-06-07 19:28:48 -0300

    file jedis

--- pila.py
+++ pila.py
@@ -21,25 +21,25 @@ class Pila(object):
         return self.__elementos[-1]
 
 
-# from random import randint
+from random import randint
 
-# pila = Pila()
-# pila_aux = Pila()
+pila = Pila()
+pila_aux = Pila()
 
-# for i in range(0, 10):
-#     pila.apilar(randint(0, 100))
+for i in range(0, 10):
+    pila.apilar(randint(0, 100))
 
-# print('cantidad de elementos', pila.tamanio())
+print('cantidad de elementos', pila.tamanio())
 
-# while(not pila.pila_vacia()):
-#     x = pila.desapilar()
-#     pila_aux.apilar(x)
-#     print(x)
+while(not pila.pila_vacia()):
+    x = pila.desapilar()
+    pila_aux.apilar(x)
+    print(x)
 
-# print()
-# while(not pila_aux.pila_vacia()):
-#     x = pila_aux.desapilar()
-#     pila.apilar(x)
+print()
+while(not pila_aux.pila_vacia()):
+    x = pila_aux.desapilar()
+    pila.apilar(x)
 
-# print()
-# print(pila.tamanio())
+print()
+print(pila.tamanio())
